285a5a25aab4b292d5557983e18b1e793a8999a8
Chinmay Patil <chinmaytlc@gmail.com>
2024-11-05 09:39:18 -0700

    Initial work (oax + bahsei cone)

---
+++ settings.lua
@@ -0,0 +1,51 @@
+local omr = OneMorRockgrove
+
+function omr.settings.createSettings()
+	local vars = omr.vars
+
+	local panelName = "OneMorRockgroveSettingsPanel"
+	local panelData = {
+		type = "panel",
+		name = "|cEFEBBEOne M0R Rockgrove Helper|r",
+		author = "|c0DC1CF@M0R_Gaming|r",
+		slashCommand = "/omr"
+	}
+
+	local optionsTable = {
+		{
+			type = "header",
+			name = "|cFFD700Oaxiltso|r",
+		},
+		{
+			type = "checkbox",
+			name = "Enable Oax Safe Zone Indicator",
+			tooltip = "If this is enabled, being outside of a safe zone for oax poisons will tint the edge of your screen red.",
+			getFunc = function() return vars.enableRedIndicator end,
+			setFunc = function(value) vars.enableRedIndicator = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Swap Oax Safe Zone Indicator",
+			tooltip = "If this is enabled, being inside of a safe zone will turn your screen red instead of the other way around.",
+			getFunc = function() return vars.reverseRed end,
+			setFunc = function(value) vars.reverseRed = value end,
+			width = "half",
+		},
+		{
+			type = "checkbox",
+			name = "Show Oax Safe Zone Borders",
+			tooltip = "If this is enabled, the safe zone boundaries for oax will show up little markers on the ground",
+			getFunc = function() return vars.showSafeBorders end,
+			setFunc = function(value) vars.showSafeBorders = value end,
+		},
+		{
+			type = "divider",
+		}
+	}
+
+
+	local panel = LibAddonMenu2:RegisterAddonPanel(panelName, panelData)
+	LibAddonMenu2:RegisterOptionControls(panelName, optionsTable)
+
+end
